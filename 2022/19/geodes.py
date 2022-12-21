b09a42bb95a43686cced2e29aefaa9483a86a8f0
Mike Rotondo <mrotondo@gmail.com>
2022-12-21 18:59:26 -0500

    day 20 and day 19 stuff

--- 2022/19/geodes.py
+++ 2022/19/geodes.py
@@ -1,5 +1,6 @@
 import re
 import copy
+import sys
 
 f = open('test_input.txt')
 blueprints = []
@@ -24,10 +25,6 @@ def can_build(blueprint, stocks, robot):
 def pay_for(blueprint, stocks, robot):
   for ingredient in blueprint[robot]:
     stocks[ingredient] -= blueprint[robot][ingredient]
-
-def build(blueprint, robots, stocks, robot):
-  pay_for(blueprint, stocks, robot)
-  robots[robot] += 1
 
 def add_into_second_dict(d1, d2):
   for k, v in d1.items():
@@ -84,6 +81,149 @@ def run_greedy(blueprint):
   print(stocks)
   return stocks['geo']
 
+def run_ratio(blueprint):
+  robots = {'ore': 1, 'cla': 0, 'obs': 0, 'geo': 0}
+  stocks = {'ore': 0, 'cla': 0, 'obs': 0, 'geo': 0}
+  for t in range(1, 25):
+    new_robots = {'ore': 0, 'cla': 0, 'obs': 0, 'geo': 0}
+    
+    geo_ratio_goal = blueprint['geo']['ore'] / blueprint['geo']['obs']
+    geo_ratio_prod = robots['ore'] / robots['obs'] if robots['obs'] > 0 else sys.maxsize
+
+    obs_ratio_goal = blueprint['obs']['ore'] / blueprint['obs']['cla']
+    obs_ratio_prod = robots['ore'] / robots['cla'] if robots['cla'] > 0 else sys.maxsize
+
+    if can_build(blueprint, stocks, 'geo'):
+      print(f't: {t}, building a geode robot')
+      pay_for(blueprint, stocks, 'geo')
+      new_robots['geo'] = 1
+    elif geo_ratio_prod > geo_ratio_goal and can_build(blueprint, stocks, 'obs'):
+      print(f't: {t}, building an obsidian robot')
+      pay_for(blueprint, stocks, 'obs')
+      new_robots['obs'] = 1
+    elif obs_ratio_prod > obs_ratio_goal and can_build(blueprint, stocks, 'cla'):
+      print(f't: {t}, building a clay robot')
+      pay_for(blueprint, stocks, 'cla')
+      new_robots['cla'] = 1
+    elif can_build(blueprint, stocks, 'ore'):
+      print(f't: {t}, building an ore robot')
+      pay_for(blueprint, stocks, 'ore')
+      new_robots['ore'] = 1
+    add_into_second_dict(robots, stocks)
+    add_into_second_dict(new_robots, robots)
+    print(f'end of t: {t}, stocks: {stocks}')
+
+  print(f'robots: {robots}')
+  print(f'stocks: {stocks}')
+  return stocks['geo']
+
+def run_quota(blueprint):
+  robots = {'ore': 1, 'cla': 0, 'obs': 0, 'geo': 0}
+  stocks = {'ore': 0, 'cla': 0, 'obs': 0, 'geo': 0}
+  obs_needed_for_one_geo = blueprint['geo']['obs']
+  cla_needed_for_one_geo = blueprint['obs']['cla'] * blueprint['geo']['obs']
+  ore_needed_for_one_geo = blueprint['geo']['ore'] + blueprint['geo']['obs'] * blueprint['obs']['ore'] + blueprint['obs']['cla'] * blueprint['cla']['ore']
+
+  norm_denominator = min([obs_needed_for_one_geo, cla_needed_for_one_geo, ore_needed_for_one_geo])
+  obs_needed_for_one_geo /= norm_denominator
+  cla_needed_for_one_geo /= norm_denominator
+  ore_needed_for_one_geo /= norm_denominator
+
+  print(f'obs_needed_for_one_geo: {obs_needed_for_one_geo}')
+  print(f'cla_needed_for_one_geo: {cla_needed_for_one_geo}')
+  print(f'ore_needed_for_one_geo: {ore_needed_for_one_geo}')
+  for t in range(1, 25):
+    print(f'starting t {t} ------------')
+    new_robots = {'ore': 0, 'cla': 0, 'obs': 0, 'geo': 0}
+    
+    needed = {
+      'obs': obs_needed_for_one_geo - (robots['obs'] % obs_needed_for_one_geo),
+      'cla': cla_needed_for_one_geo - (robots['cla'] % cla_needed_for_one_geo),
+      'ore': ore_needed_for_one_geo - (robots['ore'] % ore_needed_for_one_geo),
+    }
+    print(f'needed: {needed}')
+    satisfaction_levels = {
+      'obs': robots['obs'] // obs_needed_for_one_geo,
+      'cla': robots['cla'] // cla_needed_for_one_geo,
+      'ore': robots['ore'] // ore_needed_for_one_geo,
+      }
+    print(f'satisfaction levels: {satisfaction_levels}')
+    max_satisfaction_level = max(satisfaction_levels.values())
+    print(f'max satisfaction level: {max_satisfaction_level}')
+    needed = {k:v for k,v in needed.items() if satisfaction_levels[k] <= max_satisfaction_level}
+    print(f'filtered needed: {needed}')
+
+    next_robot = max(needed, key=needed.get)
+
+    print(f't: {t}, next robot: {next_robot}')
+
+    if can_build(blueprint, stocks, 'geo'):
+      print(f't: {t}, building a geode robot')
+      pay_for(blueprint, stocks, 'geo')
+      new_robots['geo'] += 1
+    if can_build(blueprint, stocks, next_robot):
+      print(f't: {t}, building a {next_robot} robot')
+      pay_for(blueprint, stocks, next_robot)
+      new_robots[next_robot] += 1
+    add_into_second_dict(robots, stocks)
+    add_into_second_dict(new_robots, robots)
+    print(f'end of t: {t}, stocks: {stocks}')
+
+  print(f'robots: {robots}')
+  print(f'stocks: {stocks}')
+  return stocks['geo']
+
+def run_priority(blueprint):
+  print(f'===============')
+  print(f'running priority with blueprint {blueprint}')
+  robots = {'ore': 1, 'cla': 0, 'obs': 0, 'geo': 0}
+  stocks = {'ore': 0, 'cla': 0, 'obs': 0, 'geo': 0}
+  for t in range(1, 25):
+    print(f't is {t}')
+    new_robots = {'ore': 0, 'cla': 0, 'obs': 0, 'geo': 0}
+    # decide
+    priority = None # wait by default
+    if can_build(blueprint, stocks, 'geo'):
+      priority = 'geo'
+    else:
+      # can't afford geode robot
+      geo_ore_prod_to_cost_ratio = robots['ore'] / blueprint['geo']['ore']
+      geo_obs_prod_to_cost_ratio = (robots['obs'] + 1) / blueprint['geo']['obs']
+      print(f'comparing geo_ore_prod_to_cost_ratio {geo_ore_prod_to_cost_ratio} to geo_obs_prod_to_cost_ratio {geo_obs_prod_to_cost_ratio}')
+      if geo_ore_prod_to_cost_ratio <= geo_obs_prod_to_cost_ratio:
+        priority = 'ore'
+      else:
+        priority = 'obs'  # we're not producing enough obsidian to build geode robots
+        if not can_build(blueprint, stocks, 'obs'):
+          obs_ore_prod_to_cost_ratio = robots['ore'] / blueprint['obs']['ore']
+          obs_cla_prod_to_cost_ratio = (robots['cla'] + 1) / blueprint['obs']['cla']
+          print(f'comparing obs_ore_prod_to_cost_ratio {obs_ore_prod_to_cost_ratio} to {obs_cla_prod_to_cost_ratio}')
+          if obs_ore_prod_to_cost_ratio <= obs_cla_prod_to_cost_ratio:
+            priority = None #'ore'
+          else:
+            priority = 'cla'  # we're not producing enough clay to build obsidian robots
+      
+    # pay for & start building robots
+    print(f'priority is {priority}')
+    if priority and can_build(blueprint, stocks, priority):
+      print(f'start of t: {t}, building {priority}')
+      pay_for(blueprint, stocks, priority)
+      new_robots[priority] = 1
+
+    # robots extract
+    add_into_second_dict(robots, stocks)
+
+    # finish building robots
+    add_into_second_dict(new_robots, robots)
+    
+    print(f'end of t: {t}, robots: {robots}, stocks: {stocks}')
+    print(f'----')
+
+  return stocks['geo']
+
+
 for i, blueprint in enumerate(blueprints):
   # print(find_earliest_geo(blueprint))
-  print(i * run_greedy(blueprint))
+  # print((i + 1) * run_ratio(blueprint))
+  # print((i + 1) * run_quota(blueprint))
+  print((i + 1) * run_priority(blueprint))
--- 2022/20/encrypt.py
+++ 2022/20/encrypt.py
@@ -37,7 +37,7 @@ def print_nodes(start):
 
 # key = 1
 key = 811589153
-f = open('test_input.txt')
+f = open('input.txt')
 nodes = []
 for line in f:
   i = int(line.strip())
@@ -53,41 +53,21 @@ zero_node = start_node
 while zero_node.value != 0:
   zero_node = zero_node.next
 
-print('here we go')
-print_nodes(zero_node)
-print('done')
-
 for i in range(1, 11):
-  current_node = start_node
-  next_to_move = start_node
-  num_moved = 0
-  while num_moved < len(nodes):
-    next_to_move = current_node.next
-    if not current_node.moved:
-      to_move = current_node.value
-      to_move %= len(nodes) - 1
-      move_dir = 1
-      move_func = Node.next
-      if to_move < 0:
-        move_func = Node.prev
-        move_dir = -1
-      target_node = current_node.prev
-      current_node.pop()
-      for _ in range(0, to_move, move_dir):
-        target_node = move_func(target_node)
-      # print(f'trying to insert {current_node} after {target_node}')
-      if target_node != current_node:
-        current_node.insert_after(target_node)
-        current_node.moved = True
-      num_moved += 1
-
-    current_node = next_to_move
-  
-  print(f'after {i} rounds of mixing...')
-  print_nodes(zero_node)
-  print('---')
   for node in nodes:
-    node.moved = False
+    to_move = node.value
+    to_move %= len(nodes) - 1
+    move_dir = 1
+    move_func = Node.next
+    if to_move < 0:
+      move_func = Node.prev
+      move_dir = -1
+    target_node = node.prev
+    node.pop()
+    for _ in range(0, to_move, move_dir):
+      target_node = move_func(target_node)
+    if target_node != node:
+      node.insert_after(target_node)
 
 current_node = zero_node
 checks = [1000, 2000, 3000]
