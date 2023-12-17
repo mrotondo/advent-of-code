4ae42320235e5aa92b089afeb419e9def719be14
Mike Rotondo <mrotondo@gmail.com>
2023-12-16 19:17:17 -0800

    day 11 part 2

--- 2023/11/galaxies.py
+++ 2023/11/galaxies.py
@@ -1,13 +1,5 @@
 import re
 
-def insert_row(after_i, image):
-  width = len(image[0])
-  image.insert(after_i + 1, ['.'] * width)
-
-def insert_col(after_i, image):
-  for row in image:
-    row.insert(after_i + 1, '.')
-
 f = open('input.txt')
 image = []
 
@@ -15,14 +7,18 @@ for line in f.readlines():
   row = list(line.strip())
   image.append(row)
 
-for row_i, row in reversed(list(enumerate(image))):
+expansion_amount = 999999
+empty_rows = set()
+empty_cols = set()
+
+for row_i, row in enumerate(image):
   if all([x == '.' for x in row]):
-    insert_row(row_i, image)
+    empty_rows.add(row_i)
 
 width = len(image[0])
-for col_i in [x - 1 for x in range(width, 0, -1)]:
+for col_i in range(width):
   if all([x == '.' for row in image for x in row[col_i]]):
-    insert_col(col_i, image)
+    empty_cols.add(col_i)
 
 galaxies = set()
 for row_i, row in enumerate(image):
@@ -34,6 +30,14 @@ total_distance = 0
 while len(galaxies) > 0:
   galaxy = galaxies.pop()
   for other_galaxy in galaxies:
-    total_distance += abs(other_galaxy[0] - galaxy[0]) + abs(other_galaxy[1] - galaxy[1])
+    x_span = (min(other_galaxy[0], galaxy[0]), max(other_galaxy[0], galaxy[0]))
+    y_span = (min(other_galaxy[1], galaxy[1]), max(other_galaxy[1], galaxy[1]))
+    spanned_empty_cols = list(filter(lambda x: x in range(x_span[0] + 1, x_span[1]), empty_cols))
+    spanned_empty_rows = list(filter(lambda y: y in range(y_span[0] + 1, y_span[1]), empty_rows))
+
+    distance = x_span[1] - x_span[0] + y_span[1] - y_span[0]
+    
+    distance += expansion_amount * len(spanned_empty_cols) + expansion_amount * len(spanned_empty_rows)
+    total_distance += distance
 
 print(total_distance)
