0bd028a7c972b8e95265b313ef2e91cdea8b30b8
Mike Rotondo <mrotondo@gmail.com>
2023-12-17 23:22:59 -0500

    day 13 part 2

--- 2023/13/mirrors.py
+++ 2023/13/mirrors.py
@@ -1,33 +1,36 @@
 f = open('input.txt')
 
 def find_reflection(pattern):
+  num_differences_target = 0
+  #part 2
+  num_differences_target = 1
   # try to find a vertical reflection
   for reflection_col_i in range(1, len(pattern[0])):
-    # reflection_col_i is the first row after the possible reflection line
-    reflection_found = True
+    # reflection_col_i is the first col after the possible reflection line
+    num_differences = 0
     for check_col_i in range(0, reflection_col_i):
       low_check_col_i = reflection_col_i - check_col_i - 1
       high_check_col_i = reflection_col_i + check_col_i
       if low_check_col_i < 0 or high_check_col_i >= len(pattern[0]):
         break
-      if [row[low_check_col_i] for row in pattern] != [row[high_check_col_i] for row in pattern]:
-        reflection_found = False
-        break
-    if reflection_found:
+      low_col = [row[low_check_col_i] for row in pattern]
+      high_col = [row[high_check_col_i] for row in pattern]
+      num_differences += sum([1 if low_col[i] != high_col[i] else 0 for i in range(len(pattern))])
+    if num_differences == num_differences_target:
       return (reflection_col_i, 'vertical')
   # try to find a horizontal reflection
   for reflection_row_i in range(1, len(pattern)):
     # reflection_row_i is the first row after the possible reflection line
-    reflection_found = True
+    num_differences = 0
     for check_row_i in range(0, reflection_row_i):
       low_check_row_i = reflection_row_i - check_row_i - 1
       high_check_row_i = reflection_row_i + check_row_i
       if low_check_row_i < 0 or high_check_row_i >= len(pattern):
         break
-      if pattern[low_check_row_i] != pattern[high_check_row_i]:
-        reflection_found = False
-        break
-    if reflection_found:
+      low_row = pattern[low_check_row_i]
+      high_row = pattern[high_check_row_i]
+      num_differences += sum([1 if low_row[i] != high_row[i] else 0 for i in range(len(pattern[0]))])
+    if num_differences == num_differences_target:
       return (reflection_row_i, 'horizontal')
 
 total = 0
