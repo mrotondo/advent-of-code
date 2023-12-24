fdae661d4f78b2adbc10ac48c62eb91f8b3c8a3c
Mike Rotondo <mrotondo@gmail.com>
2023-12-24 01:16:51 -0500

    troll attempt at day 14 part 2

--- 2023/14/rocks.py
+++ 2023/14/rocks.py
@@ -1,15 +1,50 @@
 import re
 
-f = open('input.txt')
+f = open('test_input.txt')
 
 platform = []
 for line in f.readlines():
   platform.append(list(line.strip()))
-  for row_i in range(len(platform) - 1, 0, -1):
-    for col_i in range(len(platform[0])):
-      if platform[row_i][col_i] == 'O' and platform[row_i - 1][col_i] == '.':
-        platform[row_i - 1][col_i] = 'O'
-        platform[row_i][col_i] = '.'
+
+def tilt_north():
+  for row_max in range(1, len(platform)):
+    for row_i in range(row_max, 0, -1):
+      for col_i in range(len(platform[0])):
+        if platform[row_i][col_i] == 'O' and platform[row_i - 1][col_i] == '.':
+          platform[row_i - 1][col_i] = 'O'
+          platform[row_i][col_i] = '.'
+
+def tilt_west():
+  for col_max in range(1, len(platform[0])):
+    for col_i in range(col_max, 0, -1):
+      for row_i in range(len(platform)):
+        if platform[row_i][col_i] == 'O' and platform[row_i][col_i - 1] == '.':
+          platform[row_i][col_i - 1] = 'O'
+          platform[row_i][col_i] = '.'
+
+def tilt_south():
+  for row_min in range(len(platform), 0, -1):
+    for row_i in range(row_min - 1, len(platform) - 1):
+      for col_i in range(len(platform[0])):
+        if platform[row_i][col_i] == 'O' and platform[row_i + 1][col_i] == '.':
+          platform[row_i + 1][col_i] = 'O'
+          platform[row_i][col_i] = '.'
+
+def tilt_east():
+  for col_min in range(len(platform[0]), 0, -1):
+    for col_i in range(col_min - 1, len(platform[0]) - 1):
+      for row_i in range(len(platform)):
+        if platform[row_i][col_i] == 'O' and platform[row_i][col_i + 1] == '.':
+          platform[row_i][col_i + 1] = 'O'
+          platform[row_i][col_i] = '.'
+
+for i in range(1000000000):
+  if i % 1000 == 0:
+    print(i)
+  tilt_north()
+  tilt_west()
+  tilt_south()
+  tilt_east()
 
 print('\n'.join(map(lambda row: ''.join([x for x in row]), platform)))
 
