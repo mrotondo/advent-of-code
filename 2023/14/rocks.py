c2e4940449931d2f41a607e3bdf147ca7bf5fc6a
Mike Rotondo <mrotondo@gmail.com>
2023-12-24 02:46:30 -0500

    its always loop detection

--- 2023/14/rocks.py
+++ 2023/14/rocks.py
@@ -1,6 +1,6 @@
 import re
 
-f = open('test_input.txt')
+f = open('input.txt')
 
 def scan_for_wall(row_i, col_i, row_inc, col_inc, platform):
   row_to_check = row_i
@@ -51,23 +51,16 @@ DIRECTION_INCREMENTS = {'north': (-1, 0),
 def tilt(tilt_direction, platform, nearest_walls):
   row_inc, col_inc = DIRECTION_INCREMENTS[tilt_direction]
   for (rock_row, rock_col) in find_rocks(tilt_direction):
-    # print('looking for nearest walls for rock at {}, {}'.format(rock_row, rock_col))
     (wall_row, wall_col) = nearest_walls[(rock_row, rock_col)][tilt_direction]
-    # print('nearest wall is at {}, {}'.format(wall_row, wall_col))
     if wall_row - row_inc == rock_row and wall_col - col_inc == rock_col:
-      # print('already against the wall, not moving')
       continue
     while (wall_row != rock_row or wall_col != rock_col) and platform[wall_row - row_inc][wall_col - col_inc] == 'O':
-      # print('but there is already a rock at {}, {}'.format(wall_row - row_inc, wall_col - col_inc))
       wall_row -= row_inc
       wall_col -= col_inc
     if wall_row == rock_row and wall_col == rock_col:
-      # print('already in a stack of rocks that is against a wall, not moving')
       continue
-    # print('moving to new location {}, {}'.format(wall_row - row_inc, wall_col - col_inc))
     platform[wall_row - row_inc][wall_col - col_inc] = 'O'
     if wall_row - row_inc != rock_row or wall_col - col_inc != rock_col:
-      # print('setting old location to empty')
       platform[rock_row][rock_col] = '.'
 
 platform = []
@@ -75,17 +68,32 @@ for line in f.readlines():
   platform.append(list(line.strip()))
 
 nearest_walls = find_nearest_walls(platform)
+
+history_index = {}
+totals = []
 for i in range(1000000000):
-  if i % 1000 == 0:
-    print(i)
+  signature = tuple(map(lambda row: ''.join(row), platform))
+  total = 0
+  for row_i, row in enumerate(platform):
+    total += sum(1 if x == 'O' else 0 for x in row) * (len(platform) - row_i)
+  if signature in history_index:
+    loop_start = history_index[signature]
+    loop_length = i - loop_start
+    num_loops = (1000000000 - loop_start) // loop_length
+    index_in_loop = 1000000000 - (loop_start + num_loops * loop_length)
+    final_index = loop_start + index_in_loop
+    print(totals[final_index])
+    exit(0)
+  history_index[signature] = i
+  totals.append(total)
   tilt('north', platform, nearest_walls)
   tilt('west', platform, nearest_walls)
   tilt('south', platform, nearest_walls)
   tilt('east', platform, nearest_walls)
 
-print('\n'.join(map(lambda row: ''.join([x for x in row]), platform)))
+# print('\n'.join(map(lambda row: ''.join([x for x in row]), platform)))
 
-total = 0
-for row_i, row in enumerate(platform):
-  total += sum(1 if x == 'O' else 0 for x in row) * (len(platform) - row_i)
-print(total)
+# total = 0
+# for row_i, row in enumerate(platform):
+#   total += sum(1 if x == 'O' else 0 for x in row) * (len(platform) - row_i)
+# print(total)
