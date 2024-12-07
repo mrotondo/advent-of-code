adca7970c2fc26caf55423e6671c5211afe0bfe9
Mike Rotondo <mrotondo@gmail.com>
2024-12-07 15:38:19 -0800

    puzzle 6 part 2

--- 2024/06/solution.py
+++ 2024/06/solution.py
@@ -17,15 +17,31 @@ def move_guard(grid, guard_pos, guard_dir):
   else:
     return move_pos, guard_dir
 
+def simulate_until_loop(grid, guard_pos, guard_dir, check_for_possible_loops):
+  visited_positions = set()
+  visited_orientations = set()
+  loop_obstacle_locations = set()
+  guard_still_in_grid = True
+  while(guard_still_in_grid and (guard_pos, guard_dir) not in visited_orientations):
+    visited_positions.add(guard_pos)
+    visited_orientations.add((guard_pos, guard_dir))
+    if check_for_possible_loops:
+      obstacle_location = (guard_pos[0] + guard_dir[0], guard_pos[1] + guard_dir[1])
+      if position_is_within_grid(grid, obstacle_location) and obstacle_location not in visited_positions:
+        old_grid_value = grid[obstacle_location[1]][obstacle_location[0]]
+        if old_grid_value == ".":
+          grid[obstacle_location[1]][obstacle_location[0]] = "#"
+          _, looped, _ = simulate_until_loop(grid, guard_pos, guard_dir, False)
+          if looped:
+            loop_obstacle_locations.add(obstacle_location)
+          grid[obstacle_location[1]][obstacle_location[0]] = old_grid_value
+    guard_pos, guard_dir = move_guard(grid, guard_pos, guard_dir)
+    guard_still_in_grid = position_is_within_grid(grid, guard_pos)
+  return visited_positions, (guard_pos, guard_dir) in visited_orientations, loop_obstacle_locations
+
 f = open('input.txt')
-grid = [line.strip() for line in f.readlines()]
-
+grid = [list(line.strip()) for line in f.readlines()]
 guard_pos, guard_dir = find_guard(grid)
-visited_positions = set()
-guard_still_in_grid = True
-while(guard_still_in_grid):
-  visited_positions.add(guard_pos)
-  guard_pos, guard_dir = move_guard(grid, guard_pos, guard_dir)
-  guard_still_in_grid = position_is_within_grid(grid, guard_pos)
-
+visited_positions, looped, loop_obstacle_locations = simulate_until_loop(grid, guard_pos, guard_dir, True)
 print(len(visited_positions))
+print(len(loop_obstacle_locations))
