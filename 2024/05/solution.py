f1c8701cbca3d487c13371986cb8ff1c5c74a54b
Mike Rotondo <mrotondo@gmail.com>
2024-12-07 14:09:25 -0800

    finish day 5

--- 2024/05/solution.py
+++ 2024/05/solution.py
@@ -1,3 +1,19 @@
+def is_valid(update, ordering_rules):
+  for before, after in ordering_rules:
+    if before in update and after in update:
+      if update.index(before) > update.index(after):
+        return False
+  return True
+
+def fix_invalid_pages(update, ordering_rules):
+  made_changes = False
+  for before, after in ordering_rules:
+    if before in update and after in update:
+      if update.index(before) > update.index(after):
+        made_changes = True
+        update.insert(update.index(after), update.pop(update.index(before)))
+  return made_changes
+
 f = open('input.txt')
 lines = [line.strip() for line in f.readlines()]
 
@@ -7,12 +23,15 @@ updates = [list(map(int, line.split(","))) for line in lines if "," in line]
 
 total = 0
 for update in updates:
-  valid = True
-  for before, after in ordering_rules:
-    if before in update and after in update:
-      if update.index(before) > update.index(after):
-        valid = False
-  if valid:
+  if is_valid(update, ordering_rules):
     total += update[int(len(update) / 2)]
 print(total)
 
+total = 0
+for update in updates:
+  if not is_valid(update, ordering_rules):
+    made_changes = fix_invalid_pages(update, ordering_rules)
+    while made_changes:
+      made_changes = fix_invalid_pages(update, ordering_rules)
+    total += update[int(len(update) / 2)]
+print(total)
