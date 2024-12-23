cee0dd7ce5c504b8de6b33ab101e1dd587a16bc4
Mike Rotondo <mrotondo@codeforamerica.org>
2024-12-23 00:18:59 -0700

    puzzle 11 part 2

--- 2024/11/solution.py
+++ 2024/11/solution.py
@@ -1,5 +1,7 @@
 import math
 
+transformations = {}
+
 def num_digits(x):
   return math.floor(math.log10(x)) + 1
 
@@ -9,24 +11,37 @@ def split_in_half(x):
   second_half = int(x - first_half * pow(10, length / 2))
   return [first_half, second_half]
 
-def transform(stone):
+def transform_one_step(stone):
+  transformation = None
   if stone == 0:
-    return [1]
+    transformation = [1]
   elif num_digits(stone) % 2 == 0:
-    return split_in_half(stone)
+    transformation =  split_in_half(stone)
   else:
-    return [stone * 2024]
+    transformation =  [stone * 2024]
+  return transformation
+
+def transform(stone, steps):
+  if steps == 0:
+    return 1
+  elif (stone, steps) in transformations:
+    return transformations[(stone, steps)]
+  else:
+    transformation = transform_one_step(stone)
+    result = sum([transform(x, steps - 1) for x in transformation])
+    transformations[(stone, steps)] = result
+    return result
 
 def flatten(xss):
   return [x for xs in xss for x in xs]
 
-def blink(stones):
-  return flatten([transform(stone) for stone in stones])
-
 input_file = open('input.txt')
 stones = list(map(int, input_file.read().strip().split(" ")))
 
-for i in range(25):
-  stones = blink(stones)
+stones = [(stone, 75) for stone in stones]
 
-print(len(stones))
+result = 0
+for stone, steps in stones:
+  result += transform(stone, steps)
+
+print(result)
