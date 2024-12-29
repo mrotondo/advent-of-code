8fd4f94e5823ee7a3505de82f551d875e9c6bd0b
Mike Rotondo <mrotondo@codeforamerica.org>
2024-12-28 17:16:03 -0700

    puzzle 13 part 2

--- 2024/13/solution.py
+++ 2024/13/solution.py
@@ -1,15 +1,15 @@
 import re
-import math
 
 def cheapest_combination(a_vector, b_vector, prize_coords):
-  combinations = []
-  for a_presses in range(101):
-    for b_presses in range(101):
-      if a_vector[0] * a_presses + b_vector[0] * b_presses == prize_coords[0] and a_vector[1] * a_presses + b_vector[1] * b_presses == prize_coords[1]:
-        combinations.append((a_presses, b_presses))
-  costs = [a_presses * 3 + b_presses for a_presses, b_presses in combinations]
-  if len(costs) > 0:
-    return min(costs)
+  a = b_vector[1] / a_vector[1]
+  b = b_vector[0] / a_vector[0]
+  c = prize_coords[0] / a_vector[0]
+  d = prize_coords[1] / a_vector[1]
+
+  b_presses = round((d - c) / (a - b))
+  a_presses = round((-b_presses * b_vector[0] + prize_coords[0]) / a_vector[0])
+  if a_presses * a_vector[0] + b_presses * b_vector[0] == prize_coords[0] and a_presses * a_vector[1] + b_presses * b_vector[1] == prize_coords[1]:
+    return round(a_presses) * 3 + round(b_presses)
   else:
     return 0
 
@@ -21,6 +21,7 @@ while True:
     a_vector = list(map(int, re.match(r'Button A: X\+([0-9]+), Y\+([0-9]+)', input_file.readline()).groups()))
     b_vector = list(map(int, re.match(r'Button B: X\+([0-9]+), Y\+([0-9]+)', input_file.readline()).groups()))
     prize_coords = list(map(int, re.match(r'Prize: X=(\d+), Y=(\d+)\n', input_file.readline()).groups()))
+    prize_coords = (prize_coords[0] + 10000000000000, prize_coords[1] + 10000000000000)
     input_file.readline()
     total += cheapest_combination(a_vector, b_vector, prize_coords)
   except Exception as e:
